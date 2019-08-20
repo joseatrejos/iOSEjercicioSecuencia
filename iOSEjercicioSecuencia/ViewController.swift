e071dfe7eab463474423ca9bc3e946c58e04bd0e
Alumno <ulsa@2.local>
2019-08-20 14:33:18 -0700

    Ejercicio de la animacion de un conejo

--- iOSEjercicioSecuencia/ViewController.swift
+++ iOSEjercicioSecuencia/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    @IBAction func doTapConejo(_ sender: Any) {
+        if !imgSecuencia.isAnimating{
+            imgSecuencia.startAnimating()
+        }
+        else {
+            imgSecuencia.stopAnimating()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Arreglo de UIImage con las imagenes de la secuencia
+        var imagenesSecuenciaConejo : [UIImage] = []
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo1")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo2")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo3")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo4")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo5")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo6")!)
+        imagenesSecuenciaConejo.append(UIImage(named : "Conejo7")!)
+        
+        imgSecuencia.animationImages = imagenesSecuenciaConejo
+        imgSecuencia.animationDuration = 0.6
+        imgSecuencia.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
